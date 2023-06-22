1a3128ebdf74a47c90d86bcdaf102f3f09a13e9f
Maxime Legare <mleg1234@protonmail.com>
2023-06-22 16:05:59 -0400

    add ts-noCheck to BlockContent

--- db/schemas/blockContent.js
+++ db/schemas/blockContent.js
@@ -1,9 +1,9 @@
+// @ts-nocheck
 import { RxColumnSpacing as icon } from "react-icons/rx";
 
 import { MdOpacity as opacityIcon } from "react-icons/md";
 
 import { AiFillHighlight as highlightIcon } from "react-icons/ai";
-// @ts-nocheck
 /**
  * This is the schema definition for the rich text fields used for
  * for this blog studio. When you import it in schemas.js it can be
